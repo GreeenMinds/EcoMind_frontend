58b5f4c984484c1b10b697377b0e10b0ad66dddd
Leotens <leotens11@gmail.com>
2026-05-09 02:01:20 -0500

    feat: Add community services

---
+++ src/app/community/infrastructure/community-goals-api-endpoint.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { BaseApiEndpoint } from '../../shared/infrastructure/base-api-endpoint';
+import { CommunityGoal } from '../domain/model/community-goal.entity';
+import { CommunityGoalAssembler } from './community-goal-assembler';
+import { CommunityGoalResource, CommunityGoalResponse } from './community-goal-response';
+
+export class CommunityGoalsApiEndpoint extends BaseApiEndpoint<
+  CommunityGoal,
+  CommunityGoalResource,
+  CommunityGoalResponse,
+  CommunityGoalAssembler
+> {
+  constructor(http: HttpClient) {
+    super(
+      http,
+      `${environment.platformProviderApiBaseUrl}${environment.platformProviderCommunityGoalEndpointPath}`,
+      new CommunityGoalAssembler(),
+    );
+  }
+}
---
+++ src/app/community/infrastructure/community-members-api-endpoint.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { BaseApiEndpoint } from '../../shared/infrastructure/base-api-endpoint';
+import { CommunityMember } from '../domain/model/community-member.entity';
+import { CommunityMemberAssembler } from './community-member-assembler';
+import { CommunityMemberResource, CommunityMemberResponse } from './community-member-response';
+
+export class CommunityMembersApiEndpoint extends BaseApiEndpoint<
+  CommunityMember,
+  CommunityMemberResource,
+  CommunityMemberResponse,
+  CommunityMemberAssembler
+> {
+  constructor(http: HttpClient) {
+    super(
+      http,
+      `${environment.platformProviderApiBaseUrl}${environment.platformProviderUserEndpointPath}`,
+      new CommunityMemberAssembler(),
+    );
+  }
+}
